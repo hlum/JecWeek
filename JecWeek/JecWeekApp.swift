ef9cfb1ab440cf88498001eef8b8cd01ab3ea344
アウン <hlumaungphyo@gmail.com>
2024-11-13 18:30:14 +0900

    Initial Commit

---
+++ JecWeek/JecWeekApp.swift
@@ -0,0 +1,17 @@
+//
+//  JecWeekApp.swift
+//  JecWeek
+//
+//  Created by Hlwan Aung Phyo on 11/13/24.
+//
+
+import SwiftUI
+
+@main
+struct JecWeekApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
