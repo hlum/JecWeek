882abbd31b30371c28b16c53098ac48cf97f051d
アウン <hlumaungphyo@gmail.com>
2024-11-13 19:28:52 +0900

    Created HomeViewModel

--- JecWeek/HomeView.swift
+++ JecWeek/HomeView.swift
@@ -7,6 +7,41 @@
 
 import SwiftUI
 
+final class HomeViewModel:ObservableObject{
+    @Published var nfcData:NFCData?
+    @Published var showAlert:Bool = false
+    @Published var alertTitle:String = ""
+    
+    private let nfcManager = NFCManager()
+    
+    init() {
+        nfcManager.onCardDataUpdate = { [weak self] data,error in
+            if let error = error{
+                Task{
+                    await self?.showAlertTitle(alertTitle: error.localizedDescription)
+                }
+            }
+            guard let data = data else {
+                Task{
+                    await self?.showAlertTitle(alertTitle: "No data found")
+                }
+                return
+            }
+            self?.nfcData = data
+        }
+    }
+    
+    @MainActor
+    func showAlertTitle(alertTitle:String){
+        showAlert = true
+        self.alertTitle = alertTitle
+    }
+    
+    func scan(){
+        nfcManager.scan()
+    }
+}
+
 struct HomeView: View {
     var body: some View {
         VStack {
